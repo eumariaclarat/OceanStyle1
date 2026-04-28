2b09c4a5f88d5c8786c9a94a5bdecca52566d3b1
Jaqueline <jaqueline_fanton@estudante.sesisenai.org.br>
2026-04-28 20:39:09 -0300

    J

--- oceanstyle/src/app/app.routes.ts
+++ oceanstyle/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { Inicio } from './inicio/inicio';
+import { Biquinis } from './biquinis/biquinis';
+import { Acessorios } from './acessorios/acessorios';
+import { Roupas } from './roupas/roupas';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'inicio',component:Inicio},
+    {path:'biquinis',component:Biquinis},
+    {path:'roupas',component:Roupas},
+    {path:'acessorios',component:Acessorios}
+];
