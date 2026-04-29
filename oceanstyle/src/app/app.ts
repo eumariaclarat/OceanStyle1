39333f2ebe92f2b76f402ec76a4ffc7ae8466ae2
Jaqueline <jaqueline_fanton@estudante.sesisenai.org.br>
2026-04-28 21:39:47 -0300

    J

--- oceanstyle/src/app/app.ts
+++ oceanstyle/src/app/app.ts
@@ -2,11 +2,10 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Footer } from './footer/footer';
 import { Header } from './header/header';
-import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet,Footer,Header,Home],
+  imports: [RouterOutlet,Footer,Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
